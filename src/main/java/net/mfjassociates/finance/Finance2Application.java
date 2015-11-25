0be32b69ca0e040e64bed388d91df2aeb6e4c916
mario <mario@mfjassociates.net>
2015-11-24 22:40:44 -0500

    Initial import of Spring Boot project

---
+++ src/main/java/net/mfjassociates/finance/Finance2Application.java
@@ -0,0 +1,12 @@
+package net.mfjassociates.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finance2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Finance2Application.class, args);
+    }
+}
